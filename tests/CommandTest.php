10114242acb99d55c49c44ea484e4417eca430db
Craig Morris <craig.michael.morris@gmail.com>
2021-12-08 16:21:49 +0000

    add tests

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -5,25 +5,9 @@ use Morrislaptop\LaravelSeedList\Tests\TestClasses\DatabaseSeeder;
 use Morrislaptop\LaravelSeedList\Tests\TestClasses\Of;
 use Morrislaptop\LaravelSeedList\Tests\TestClasses\SubSeeder;
 
-it('lists, validates and runs seeders', function () {
+it('lists validates and runs seeders', function () {
     $this->artisan('db:seed', ['class' => DatabaseSeeder::class])
-        ->expectsOutput(<<<EOT
-1. AnotherSeeder
-• SubSeeder
-2. ExampleSeeder
-• AnotherSeeder
-• SubSeeder
-• Series
-• Of
-• Seeders
-3. Of
-4. Seeders
-5. Series
-6. SubSeeder
-EOT)
-        ->expectsQuestion('Which seeder(s) would you like to run? Separate multiple choices with a comma', '0,1,3')
-        ->expectsOutput('The following choices are not valid: 0')
-        ->expectsQuestion('Which seeder(s) would you like to run? Separate multiple choices with a comma', '1,3')
+        ->expectsQuestion('Which seeder would you like to run?', ['AnotherSeeder', 'Of'])
         ->expectsOutput('Seeding: ' . AnotherSeeder::class)
         ->expectsOutput('Seeding: ' . SubSeeder::class)
         // ->expectsOutput('Seeded: ' . AnotherSeeder::class . ' (0.00ms)')
